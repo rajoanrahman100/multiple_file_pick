28e2009c1667de58225a792df43b4b39df2410f1
rajoanrahman100 <rajoan@rokkhi.com>
2021-10-06 18:30:10 +0600

    file picker demo

---
+++ lib/file_page.dart
@@ -0,0 +1,59 @@
+import 'dart:developer';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+import 'package:open_file/open_file.dart';
+
+class FilePage extends StatelessWidget {
+  
+  List<PlatformFile?> file;
+  ValueChanged<PlatformFile> openFile;
+
+  FilePage({Key? key, required this.file,required this.openFile}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Selected Files"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10.0),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: ListView.builder(itemCount: file.length,itemBuilder: (_,index){
+
+            final kb=file[index]!.size/1024;
+            final mb=kb/1024;
+            final fileSize= mb>=1?'${mb.toStringAsFixed(2)} MB':'${mb.toStringAsFixed(2)} KB';
+            return Column(
+              children: [
+                ListTile(
+                  leading: Container(
+                    height: 50,width: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    child: Center(
+                      child: Text(".${file[index]!.extension}"),
+                    ),
+                  ),
+                  title: Text("${file[index]!.name}"),
+                  trailing: Text(fileSize),
+                  onTap: (){
+                    OpenFile.open(file[index]!.path);
+                  },
+                ),
+               const Divider(color: Colors.black,)
+              ],
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,87 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:file_picker_example/file_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+class HomePage extends StatelessWidget{
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("File Picker"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              MaterialButton(minWidth: 200.0,onPressed:()async{
+                
+                final results=await FilePicker.platform.pickFiles();
+                if(results==null){
+                  log("No File Pick Up");
+                  return;
+                }
+                else{
+                  final file=results.files.first;
+                  // log("Name: ${file.name}");
+                  // log("Size: ${file.size}");
+                  // log("Byte: ${file.bytes}");
+                  // log("Path: ${file.path}");
+
+                  final newFile=await saveFilePermanantly(file);
+
+                  log("new File: ${newFile!.path}");
+                  log("old File: ${file.path}");
+                  //openFile(file);
+                }
+
+              },color: Colors.amber,shape: const StadiumBorder(),child: const Text("Pick File"),),
+
+              MaterialButton(onPressed:()async{
+                final results=await FilePicker.platform.pickFiles(allowMultiple: true);
+                if(results==null){
+                  log("No File Pick Up");
+                  return;
+                }
+                else{
+                  openMultipleFile(context, results.files);
+                }
+              },color: Colors.blueAccent,minWidth: 200,shape:const StadiumBorder(),child: const Text("Pick Multiple File"),)
+            ],
+          ),
+        ),
+      ),
+
+    );
+    
+  }
+
+  void openFile(PlatformFile file){
+    OpenFile.open(file.path);
+  }
+
+  //Open multiple files
+  void openMultipleFile(BuildContext context,List<PlatformFile?> files){
+    Navigator.of(context).push(MaterialPageRoute(builder:(context)=>FilePage(file: files,openFile: openFile,)));
+  }
+
+  //Save the file permanantly
+
+  Future<File?> saveFilePermanantly(PlatformFile file)async{
+    final appStorage=await getApplicationDocumentsDirectory();
+    final newFile=File('${appStorage.path}/${file.name}');
+    return File(file.path!).copy(newFile.path);
+  }
+
+}
